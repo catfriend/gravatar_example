56c74a9320bece89369ef11decced7fe88951fc7
catfriend <eakari@live.com>
2016-01-04 18:02:39 -0800

    code added for gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def profile_image_for(user)
+    url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}"
+
+    # url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}?s=200"
+    image_tag(url, alt: user.name)
+  end
 end
+
+# Step 2: add a custom view helper that takes a user object as a parameter
+# The default size is 80 pixels. If you wish to resize, follow the commented
+# out code above with the desired pixel size. As images are square only one
+# size parameter is needed.
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
                     format: /\A\S+@\S+\z/,
                     uniqueness: { case_sensitive: false }
+
+  # Step 1: generate an MD hash of the user's email
+
+  def gravatar_id
+    Digest::MD5::hexdigest(email.downcase)
+  end
+
 end
